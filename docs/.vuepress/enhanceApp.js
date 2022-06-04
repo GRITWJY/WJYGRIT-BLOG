1ca4be0d82a43afa54b7d2b59655614fa46d86f0
WJYGRIT <1435408225@qq.com>
2022-06-04 21:08:46 +0800

    fasf

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -2,7 +2,10 @@ export default ({ router }) => {
   router.beforeEach((to, from, next) => {
     if (typeof _hmt !== "undefined") {
       if (to.path) {
-        _hmt.push(["_trackPageview", to.fullPath.split("#")[0]]);
+        if (to.fullPath.split("#").length === 1) {
+          console.log(to.fullPath);
+          _hmt.push(["_trackPageview", to.fullPath.split("#")[0]]);
+        }
       }
     }
 
