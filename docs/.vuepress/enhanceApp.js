8e68cfbe26ec48979eeef3881c40003ab5a9a806
WJYGRIT <1435408225@qq.com>
2022-06-04 20:58:47 +0800

    fasdfa

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -2,7 +2,7 @@ export default ({ router }) => {
   router.beforeEach((to, from, next) => {
     if (typeof _hmt !== "undefined") {
       if (to.path) {
-        _hmt.push(["_trackPageview", to.fullPath]);
+        _hmt.push(["_trackPageview", to.fullPath.split("#")[0]]);
       }
     }
 
