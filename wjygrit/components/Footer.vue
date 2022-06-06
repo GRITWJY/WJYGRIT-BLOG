c9962771b3c2eb30b15da5c6369935be91dde688
WJYGRIT <1435408225@qq.com>
2022-06-06 15:50:27 +0800

    补充备案信息

--- wjygrit/components/Footer.vue
+++ wjygrit/components/Footer.vue
@@ -26,6 +26,34 @@
     <a href="https://beian.miit.gov.cn/" target="_blank"
       >鄂ICP备2022008939号-1</a
     >
+    <div style="width: 300px; margin: 0 auto; padding: 20px 0">
+      <img
+        src="https://wjygrit.oss-cn-beijing.aliyuncs.com/2021/police.png"
+        alt="备案编号图标"
+      />
+      <a
+        target="_blank"
+        href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=42900402000379"
+        style="
+          display: inline-block;
+          text-decoration: none;
+          height: 20px;
+          line-height: 20px;
+        "
+        ><img src="" style="float: left" />
+        <p
+          style="
+            float: left;
+            height: 20px;
+            line-height: 20px;
+            margin: 0px 0px 0px 5px;
+            color: #939393;
+          "
+        >
+          鄂公网安备 42900402000379号
+        </p></a
+      >
+    </div>
   </div>
 </template>
 
